6d0e40045314a5a6976fb71f9ed498a00b0f67a1
Luis Díaz <luisdh.8@outlook.com>
2025-05-06 07:50:07 -0600

    update practice.py with incorrect identation practices

--- practice.py
+++ practice.py
@@ -2,3 +2,20 @@
 This is a simple Python script that applies PEP 8 style guide recommendations to a given Python file.
 Also, it works as a practice for using git command line and GitHub.
 """
+#----------------------------------------------------------
+
+# Identation = 4 spaces per level
+
+#----------------------------------------------------------
+
+# Incorrect indentation:
+
+# Arguments on first line forbidden when not using vertical alignment.
+foo = long_function_name(var_one, var_two,
+    var_three, var_four)
+
+# Further indentation required as indentation is not distinguishable.
+def long_function_name(
+    var_one, var_two, var_three,
+    var_four):
+    print(var_one)
